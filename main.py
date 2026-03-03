be39555cd75af6d98e619c7d9e8cea63c1c1c943
Jhoiker0024 <jhoikerfagundez2002@gmail.com>
2026-03-02 23:35:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.markdown(
         background-image: url("https://i.postimg.cc/VNHGhCyG/Chat-GPT-Image-2-mar-2026-10-36-51-p-m.png");
         background-size: 90%;
         background-repeat: no-repeat;
-        background-position: center 20px;
+        background-position: center 10px;
         background-color: #0f172a;
         font-family: "Courier New", Courier, monospace;
     }
